dbc5d7f17392e5c769f073abe840f30025a64b48
Youssef <youssefbahaddou2001@gmail.com>
2025-05-26 15:00:31 +0100

    First commit

---
+++ foodallergy-backend/src/main/java/ma/foodallergyai/foodallergyapi/FoodallergyBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.foodallergyai.foodallergyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodallergyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodallergyBackendApplication.class, args);
+    }
+
+}
